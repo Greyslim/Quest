0a191dad69b3b65801f8df9f1307ddabe751fc91
Greyslim <kalinin.kostik@gmail.com>
2018-07-31 16:34:00 +0300

    Example

---
+++ index.php
@@ -0,0 +1,9 @@
+ <form action="action.php" method="post">
+ <p>Имя команды: <input type="text" name="name" /></p>
+ <p>Пароль: <input type="text" name="age" /></p>
+ <p><input type="submit" /></p>
+</form>
+
+<?php 
+
+ ?>
